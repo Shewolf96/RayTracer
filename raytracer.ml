c3d930886652f660058b209078988ab7fa992bab
Kamirus <listopadkamil@gmail.com>
2018-01-12 17:20:50 +0100

    ObjectInstance

--- raytracer.ml
+++ raytracer.ml
@@ -3,9 +3,9 @@
 
 type point = float * float * float
 
-type direction = point
+type vector = point
 
-type vector = point * direction
+type ray = point * vector
 
 type color = int * int * int
 type normalized_color = point
@@ -15,13 +15,10 @@ module type OBJECT =
 sig
 
     type t
-    type position
-    type parameters
+    type config
 
-    val get_position : t -> position
-    val get_parameters : t -> parameters
-    val create : position -> parameters -> t
-    val intersect : vector -> (point * parameters) -> (float * point) option
+    val create : config -> t
+    val intersect : ray -> t -> (float * point) option
 end
 
 
@@ -39,65 +36,114 @@ end
 module type ObjectInstance =
 sig
 
-    module OBJECT : OBJECT
-    val this : OBJECT.t
-end
-(*
-module type PLACE =
-sig
-    type t
-
     module O : OBJECT
-    type obj = O.t
-
-    module L : LIGHT
-    type light = L.t
-
-    val closest_intersection : vector -> obj list -> obj *  point (* jak tu dać typ modułu OBJECT zamiast 'object'? *)
+    val this : O.t
 end
 
- *)
-(* _____________________________________________________________________________ *)
+let make_obj_instance (type a) (module Obj : OBJECT with type config = a) (obj_c) =
+    (module struct
+        module O = Obj
+        let this = O.create obj_c
+    end : ObjectInstance) 
+    
+    
+    (*
+    module type PLACE =
+    sig
+        type t
+        
+        module O : OBJECT
+        type obj = O.t
+        
+        module L : LIGHT
+        type light = L.t
+        
+        val closest_intersection : vector -> obj list -> obj *  point (* jak tu dać typ modułu OBJECT zamiast 'object'? *)
+    end
+    
+    *)
+    (* _____________________________________________________________________________ *)
+    
+    type sphere_t = {position : point; r : float}
+    
+    module Sphere : OBJECT with type config = sphere_t =
+    struct
+        
+        type config = sphere_t
+        type t = sphere_t
+        
+        let delta_t a b c = b*.b -. 4.*.a*.c
+        
+        let solutions_t a b c = let delta = delta_t a b c in
+        if delta < 0. then []
+        else if delta = 0. then [-.b/.2.*.a]
+        else let sq = Pervasives.sqrt(delta) in [(-.b -. sq)/.2.*.a; (-.b +. sq)/.2.*.a]
+        
+        let min_t_solution = function
+        | [] -> None
+        | t1::t2::_ -> if t1 < t2 && t1 > 0. then Some (t1, (0.,0.,0.))
+        else if t2 > 0. then Some (t2, (0.,0.,0.))
+        else None
+        | t'::_ -> Some (t', (0.,0.,0.)) (* jak policzyc ten punkt????? *)
+        
+        let create t = t
+        
+        let intersect vec {position=pos; r} = 
+        let (x0, y0, z0) = pos and ((p1, p2, p3), (v1, v2, v3)) = vec in
+        let a = v1 *. v1 +. v2 *. v2 +. v3 *. v3
+        and b = 2.*.p1*.v1 +. 2.*.p2*.v2 +. 2.*.p3*.v3 -. 2.*.v2*.y0 -. 2.*.v3*.z0 -. 2.*.v1*.x0
+        and c = p1*.p1 +. p2*.p2 +. p3*.p3 -. 2.*.p1*.x0 -. 2.*.p2*.y0 -. 2.*.p3*.z0 +. x0*.x0 +. y0*.y0 +. z0*.z0 in
+        min_t_solution (solutions_t a b c)
+        
+        
+    end
+    
+    module Sphere2 : OBJECT with type config = sphere_t =
+    struct
+        
+        type config = sphere_t
+        type t = sphere_t
+        
+        let delta_t a b c = b*.b -. 4.*.a*.c
+        
+        let solutions_t a b c = let delta = delta_t a b c in
+        if delta < 0. then []
+        else if delta = 0. then [-.b/.2.*.a]
+        else let sq = Pervasives.sqrt(delta) in [(-.b -. sq)/.2.*.a; (-.b +. sq)/.2.*.a]
+        
+        let min_t_solution = function
+        | [] -> None
+        | t1::t2::_ -> if t1 < t2 && t1 > 0. then Some (t1, (0.,0.,0.))
+        else if t2 > 0. then Some (t2, (0.,0.,0.))
+        else None
+        | t'::_ -> Some (t', (0.,0.,0.)) (* jak policzyc ten punkt????? *)
+        
+        let create t = t
+        
+        let intersect vec {position=pos; r} = 
+        let (x0, y0, z0) = pos and ((p1, p2, p3), (v1, v2, v3)) = vec in
+        let a = v1 *. v1 +. v2 *. v2 +. v3 *. v3
+        and b = 2.*.p1*.v1 +. 2.*.p2*.v2 +. 2.*.p3*.v3 -. 2.*.v2*.y0 -. 2.*.v3*.z0 -. 2.*.v1*.x0
+        and c = p1*.p1 +. p2*.p2 +. p3*.p3 -. 2.*.p1*.x0 -. 2.*.p2*.y0 -. 2.*.p3*.z0 +. x0*.x0 +. y0*.y0 +. z0*.z0 in
+        min_t_solution (solutions_t a b c)
+        
+        
+    end
+    
+    let ray1 = ((0.,0.,0.),(1.,1.,1.))
 
+    let l = 
+      let s = make_obj_instance (module Sphere) {position = (0.,0.,0.); r = 1.}
+      and s2 = make_obj_instance (module Sphere2) {position = (0.,0.,0.); r = 2.} in
+      [s;s2]
+    
+      let tescik = match l with
+                    (module OInstance)::t -> OInstance.O.intersect ray1 OInstance.this
+                    | _ -> failwith ""
 
-module Sphere : OBJECT with type position = point and type parameters = float list  =
-struct
-
-    type position = point
-    type parameters = float list
-    type t = point * parameters
-
-
-    let get_parameters (pos, param) = param
-    let get_position (pos, param) = pos
-
-    let delta_t a b c = b*.b -. 4.*.a*.c
-
-    let solutions_t a b c = let delta = delta_t a b c in
-                            if delta < 0. then []
-                            else if delta = 0. then [-.b/.2.*.a]
-                                else let sq = Pervasives.sqrt(delta) in [(-.b -. sq)/.2.*.a; (-.b +. sq)/.2.*.a]
-
-    let min_t_solution = function
-                            | [] -> None
-                            | t1::t2::_ -> if t1 < t2 && t1 > 0. then Some (t1, (0.,0.,0.))
-                                            else if t2 > 0. then Some (t2, (0.,0.,0.))
-                                                else None
-                            | t'::_ -> Some (t', (0.,0.,0.)) (* jak policzyc ten punkt????? *)
-
-    let create pos param = (pos, param)
-
-    let intersect (vec:vector) ((pos:point), (param:parameters)) = let (x0, y0, z0) = pos and r = List.hd param and ((p1, p2, p3), (v1, v2, v3)) = vec in
-                                let a = v1 *. v1 +. v2 *. v2 +. v3 *. v3
-                                and b = 2.*.p1*.v1 +. 2.*.p2*.v2 +. 2.*.p3*.v3 -. 2.*.v2*.y0 -. 2.*.v3*.z0 -. 2.*.v1*.x0
-                                and c = p1*.p1 +. p2*.p2 +. p3*.p3 -. 2.*.p1*.x0 -. 2.*.p2*.y0 -. 2.*.p3*.z0 +. x0*.x0 +. y0*.y0 +. z0*.z0 in
-                            min_t_solution (solutions_t a b c)
-
-
-end
-
-
-module Sun : LIGHT =
+        
+        
+        module Sun : LIGHT =
 struct
 
     type position = direction
